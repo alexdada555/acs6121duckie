08c04a474437323a0a527cce92d2a17b4952a4bd
alexdada555 <alexdada555@gmail.com>
2020-03-06 13:04:59 +0000

    Commit notes

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,5 @@
 # parameters
-ARG REPO_NAME="<REPO_NAME_HERE>"
+ARG REPO_NAME="acs6121duckie"
 
 # ==================================================>
 # ==> Do not change this code
---
+++ packages/my_package/src/my_node.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+import os
+import rospy 
+from duckietown import DTROS 
+from std_msgs.msg import String
+from duckietown_msgs.msg import WheelsCmdStamped, BoolStamped
+
+class MyNode(DTROS):
+	def __init__(self, node_name):
+		# initialize the DTROS parent class
+		super(MyNode, self).__init__(node_name=node_name)
+		# construct publisher
+		self.pub = rospy.Publisher('/duckiebot7/wheels_driver_node/wheels_cmd', WheelsCmdStamped ,queue_size=1)
+		
+	def run(self):
+		# publish message every 1 second
+		rate = rospy.Rate(1) 
+		# 1Hz    
+	
+		while not rospy.is_shutdown():
+			msg = WheelsCmdStamped()
+			msg.header.stamp = rospy.get_rostime()
+			msg.vel_left = 0.1
+			msg.vel_right = 0.1
+
+			self.pub.publish(msg)            
+			rate.sleep()
+
+if __name__ == '__main__':    
+	# create the node    
+	node = MyNode(node_name='my_node')    
+	# run node    
+	node.run()    
+	# keep spinning    
+	rospy.spin()
+
+
